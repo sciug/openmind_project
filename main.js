7314d623dc7e0a553c175074c03fd649cf9ffcdd
sciug <gaetano.sciurti@libero.it>
2022-04-29 16:27:21 +0200

    product page carousel issue

--- main.js
+++ main.js
@@ -1,11 +1,70 @@
-let removeButton = document.getElementById('remove')
+let text_1 = document.getElementById('text_1')
 
-
-removeButton.addEventListener('click', function(){
+let removeButton_1 = document.getElementById('remove_1')
+removeButton_1.addEventListener('click', function(){
     if (this.innerHTML === "remove") {
         this.innerHTML = "add";
+        
+        text_1.style.display = "none"
+        
       } else {
         this.innerHTML = "remove";
+        text_1.style.display = "inline-block"
+       
       }
 })
+
+
+
+let text_2 = document.getElementById('text_2')
+
+let removeButton_2 = document.getElementById('remove_2')
+removeButton_2.addEventListener('click', function(){
+    if (this.innerHTML === "remove") {
+        this.innerHTML = "add";
+        
+        text_2.style.display = "none"
+        
+      } else {
+        this.innerHTML = "remove";
+        text_2.style.display = "inline-block"
+       
+      }
+})
+
+
+let text_3 = document.getElementById('text_3')
+
+let removeButton_3 = document.getElementById('remove_3')
+removeButton_3.addEventListener('click', function(){
+    if (this.innerHTML === "remove") {
+        this.innerHTML = "add";
+        
+        text_3.style.display = "none"
+        
+      } else {
+        this.innerHTML = "remove";
+        text_3.style.display = "inline-block"
+       
+      }
+})
+
+
+let text_4 = document.getElementById('text_4')
+
+let removeButton_4 = document.getElementById('remove_4')
+removeButton_4.addEventListener('click', function(){
+    if (this.innerHTML === "remove") {
+        this.innerHTML = "add";
+        
+        text_4.style.display = "none"
+        
+      } else {
+        this.innerHTML = "remove";
+        text_4.style.display = "inline-block"
+       
+      }
+})
+
+
 
