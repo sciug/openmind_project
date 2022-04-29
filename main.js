21bf71e02f35d848ac39cc6243a7328200fe5167
sciug <gaetano.sciurti@libero.it>
2022-04-29 14:01:47 +0200

    start product page

---
+++ main.js
@@ -0,0 +1,10 @@
+let removeButton = document.getElementById('remove')
+
+
+removeButton.addEventListener('click', function(){
+    if (this.innerHTML === "remove") {
+        this.innerHTML = "add";
+      } else {
+        this.innerHTML = "remove";
+      }
+})
